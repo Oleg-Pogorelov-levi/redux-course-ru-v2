94cedcddb66c471a778a9ffaf0da94ec497621d1
Oleg <debagull@gmail.com>
2019-12-18 16:43:16 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { store } from './store/configureStore'
 import { Provider } from 'react-redux'
-import App from './App'
+import App from './containers/App'
 
 import registerServiceWorker from './registerServiceWorker'
 
 import './index.css'
-
-const store = createStore(() => {}, {}) // [1]
 
 ReactDOM.render(
   <Provider store={store}>
